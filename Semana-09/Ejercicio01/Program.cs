32ef42b73538b6fa5b294d21bfacb36f5589b067
Rodrigo <rolortegui09@gmail.com>
2025-10-16 10:29:59 -0500

    Subiendo semana 9

---
+++ Semana-09/Ejercicio01/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string marca;
+            int cantidad, obsequio;
+            double precio, importeCompra, porcDescuento, importeDescuento, importePago;
+            Console.Write("Marca: ");
+            marca = Console.ReadLine();
+
+            Console.Write("Cantidad: ");
+            cantidad = int.Parse(Console.ReadLine());
+
+            // Determinando el precio
+            if (marca == "Dell")
+                precio = 11000.00;
+            else if (marca == "HP")
+                precio = 9000.00;
+            else if (marca == "Apple")
+                precio = 13000.00;
+            else    // Asus
+                precio = 12500.00;
+
+            // Calculando el importe de compra (Sin descuento)
+            importeCompra = cantidad * precio;
+
+            // Identificando el porcentaja de descuento
+            if (cantidad <= 3)
+                porcDescuento = 0.035;
+            else if (cantidad < 6)
+                porcDescuento = 0.05;
+            else if (cantidad < 9)
+                porcDescuento = 0.065;
+            else
+                porcDescuento = 0.08;
+
+            // Calcular el importe de descuento
+            importeDescuento = porcDescuento * importeCompra;
+
+            // Calculamos el importe de pago
+            importePago = importeCompra - importeDescuento;
+
+            // Calculamos el obsequio
+            if (marca == "HP")
+                obsequio = 3 * (cantidad / 3);
+            else
+                obsequio = 0;
+
+            Console.WriteLine($"Importe de compra: {importeCompra}");
+            Console.WriteLine($"Importe de descuento: {importeDescuento}");
+            Console.WriteLine($"Importe de pago: {importePago}");
+            Console.WriteLine($"Mouse pad - Obsequio: {obsequio}");
+
+
+
+        }
+    }
+}
---
+++ Semana-09/OlorteguiBazan03/OlorteguiBazan03.py
@@ -0,0 +1,25 @@
+import random
+can1=0
+can2=0
+can3=0
+can4=0
+#Generar 15 aleatorios entre 1250 a 10000
+for i in range(1,16):
+    aleatorio = random.randint(1250,10000)
+    print(aleatorio)
+    suma += aleatorio;  # Sumamos los números al
+    if 1250 <= aleatorio <= 3000:
+        can1 += 1
+    elif 3001 <= aleatorio <= 5000:
+        can2 += 1
+    elif 5001 <= aleatorio <= 6999:
+        can3 += 1
+    else:   # aleatorio >= 7000
+        can4 += 1
+
+print(f"Suma: {suma}")
+print(f"Cantidad entre 1250 y 3000: {can1}")
+print(f"Cantidad entre 3001 y 5000: {can2}")
+print(f"Cantidad entre 5001 y 6999: {can3}")
+print(f"Cantidad mayor a 7000: {can4}")
+
